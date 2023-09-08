8cad2bb52b4fb6405a8ecba14910cfc81fc423eb
unknown <shaikhshahbaz4022@gmail.com>
2023-09-08 10:04:57 +0530

    server Added

--- Backend/LMS/user_routes/urls.py
+++ Backend/LMS/user_routes/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login', views.Login, name="login"),
     path('logout', views.Logout, name='logout'),
     path('get', views.allusers, name='get'),
+
     path('home', views.homeroute, name="home")
 ]
